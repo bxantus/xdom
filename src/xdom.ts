d14591d6889699faa3b76719d7ce92b2fdf5153c
bxantus <drx.trash@gmail.com>
2021-11-02 14:30:46 +0100

    SightInfo panel without preact - visited and set destination toggles missing

--- src/xdom.ts
+++ src/xdom.ts
@@ -4,11 +4,19 @@ type TagNames = keyof HTMLElementTagNameMap
 
 interface ElementProps {
     class?: string
+    innerText?:string
+    onClick?:(ev: MouseEvent)=>void
 }
 
-export function el(tagname:TagNames, props?:ElementProps) {
+export function el(tagname:TagNames, props?:ElementProps, children?:(HTMLElement|string)[]) {
     const element = document.createElement(tagname)
     if (props?.class)
         element.className = props.class
+    if (props?.innerText)
+        element.innerText = props.innerText
+    if (props?.onClick)
+        element.onclick = props.onClick
+    if (children)
+        element.append(...children)
     return element
 } 
