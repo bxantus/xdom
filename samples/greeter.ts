ad78cf24519a911a0a6afb15a7e878b78b4b5cf1
bxantus <drx.trash@gmail.com>
2022-02-14 14:12:34 +0100

    Added greeter sample Run `deno bundle greeter.ts out/greeter.js` to build

---
+++ samples/greeter.ts
@@ -0,0 +1,28 @@
+import { div, el } from "../src/xdom.ts"
+import { binding, makeObservable } from "../src/binding/binding.ts"
+
+const greeting = makeObservable({
+    greet: "Hello",
+    user: "Word",
+})
+
+const main = el("div", { class:"main" }, [
+    el("span", { innerText: binding(()=>`${greeting.greet} ${greeting.user}!`, 
+                                         greeting.$changes.greet, greeting.$changes.user) }
+    ),
+])
+
+window.onload = () => {
+    document.body.append(main)
+    // add a bunch of buttons for easier testing
+    document.body.append(
+        div({class:"controls greet"}, 
+            el("button", { innerText:"Say 'Hi'",  onClick: ()=> greeting.greet = "Hi" }),
+            el("button", { innerText:"Say 'Welcome'",  onClick: ()=> greeting.greet = "Welcome" }),
+        ),
+        div({class:"controls user"}, 
+            el("button", { innerText:"To 'World'",  onClick: ()=> greeting.user = "Word" }),
+            el("button", { innerText:"To 'John'",  onClick: ()=> greeting.user = "John" }),
+        )
+    )
+}
